1fc697c01b87c015d0d04a92cba69a31e674fd94
zhao <jnzhao2021@163.com>
2022-06-27 11:26:22 +0800

    2022/6/27

---
+++ src/algorithm/LowestCommonAncestorOfaBinarySearchTree.java
@@ -0,0 +1,56 @@
+package src.algorithm;
+
+import src.structure.TreeNode;
+
+/**
+ * @author zjn
+ * @date 2022/6/27
+ */
+public class LowestCommonAncestorOfaBinarySearchTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while (true) {
+            if (root.val > p.val && root.val >q.val) {
+                root = root.left;
+            } else if (root.val < p.val && root.val < q.val) {
+                root = root.right;
+            } else {
+                break;
+            }
+        }
+        return root;
+    }
+}
+
+/**
+ * Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
+ *
+ * According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+ * Output: 6
+ * Explanation: The LCA of nodes 2 and 8 is 6.
+ * Example 2:
+ *
+ *
+ * Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+ * Output: 2
+ * Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
+ * Example 3:
+ *
+ * Input: root = [2,1], p = 2, q = 1
+ * Output: 2
+ *
+ *
+ * Constraints:
+ *
+ * The number of nodes in the tree is in the range [2, 105].
+ * -109 <= Node.val <= 109
+ * All Node.val are unique.
+ * p != q
+ * p and q will exist in the BST.
+ */
