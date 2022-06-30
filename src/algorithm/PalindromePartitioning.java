1c0903a7e9186cb772e363cece59cd8eb529b02e
zhao <jnzhao2021@163.com>
2022-06-30 21:39:52 +0800

    2022/6/30

---
+++ src/algorithm/PalindromePartitioning.java
@@ -0,0 +1,78 @@
+package src.algorithm;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author zjn
+ * @date 2022/6/30
+ */
+public class PalindromePartitioning {
+    List<List<String>> lists = new ArrayList<>();
+    Deque<String> deque = new LinkedList<>();
+
+    public List<List<String>> partition(String s) {
+        backTracking(s, 0);
+        return lists;
+    }
+
+    private void backTracking(String s, int startIndex) {
+        if (startIndex >= s.length()) {
+            lists.add(new ArrayList<>(deque));
+            return;
+        }
+        for (int i = startIndex; i < s.length(); i++) {
+            //如果是回文子串，则记录
+            if (isPalindrome(s, startIndex, i)) {
+                String str = s.substring(startIndex, i + 1);
+                deque.addLast(str);
+            } else {
+                continue;
+            }
+            //起始位置后移，保证不重复
+            backTracking(s, i + 1);
+            deque.removeLast();
+        }
+    }
+
+    /**
+     * 判断是否是回文串
+     * @param s
+     * @param startIndex
+     * @param end
+     * @return
+     */
+    private boolean isPalindrome(String s, int startIndex, int end) {
+        for (int i = startIndex, j = end; i < j; i++, j--) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+/**
+ * Given a string s, partition s such that every substring of the partition is a palindrome. Return all possible palindrome partitioning of s.
+ *
+ * A palindrome string is a string that reads the same backward as forward.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: s = "aab"
+ * Output: [["a","a","b"],["aa","b"]]
+ * Example 2:
+ *
+ * Input: s = "a"
+ * Output: [["a"]]
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= s.length <= 16
+ * s contains only lowercase English letters.
+ */
